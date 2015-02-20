6ca75679c8e902daf249b8565e5010bba744900e
maggieo143 <maggieo143@yahoo.com>
2015-02-20 15:31:32 -0600

    commit 3

--- HW2_Q3.m
+++ HW2_Q3.m
@@ -1,62 +1,27 @@
-for zf=[.015 .03 .045 .06 .075 ]
+zone_size=round(max(size(pointTarget.data))/5);
 
-time=[0:1:size(pointTarget.data,1)-1]*(1/((pointTarget.samplingRateMHz)*(10^6)));
+zone1=pointTarget.data(1:zone_size,:,:);
+zone2=pointTarget.data(zone_size:2*zone_size,:,:);
+zone3=pointTarget.data(2*zone_size:3*zone_size,:,:);
+zone4=pointTarget.data(3*zone_size:4*zone_size,:,:);
+zone5=pointTarget.data(4*zone_size:end,:,:);
 
-rate_upsample=((pointTarget.samplingRateMHz)*(10^6))*4;
+center=(zone_size/2)+1;
 
-time_upsample=[0:1/4:size(pointTarget.data,1)-1]*(1/((pointTarget.samplingRateMHz)*(10^6)));
+center_zone1=center;
+center_zone2=center+zone_size;
+center_zone3=center+2*zone_size;
+center_zone4=center+3*zone_size;
+center_zone5=center+4*zone_size;
 
-figure;
-for aa=1:128
-    interpolation(:,:,aa)=interp1(time,pointTarget.data(:,:,aa),time_upsample,'linear');
-    imagesc(interpolation(:,:,aa))
-    colormap('gray')
-end
+0:1/(pointTarget.samplingRateMHz*(10^6)):(size(pointTarget.data,1)-1)/(pointTarget.samplingRateMHz*(10^6));
 
-for bb=0:63
-    xe_1(bb+1)=((pointTarget.elementSpacingMM)/1000)*(64-bb);
-    diag_dist_1(bb+1)=sqrt(zf^2 + (xe_1(bb+1))^2);
-    time_diag_1(bb+1)=diag_dist_1(bb+1)/1540; 
-end
-
-for cc=0:63
-    time_delay_1(cc+1)=time_diag_1(cc+1)-time_diag_1(64);
+for zf=[(time(center_zone1)*1540)/2 (time(center_zone2)*1540)/2 (time(center_zone3)*1540)/2 (time(center_zone4)*1540)/2 (time(center_zone5)*1540)/2]
+    for bb=-63.5:1:63.5
+        xe(bb+64.5)=((pointTarget.elementSpacingMM)/1000)*bb;
+        diag_dist(bb+64.5)=sqrt(zf^2 + (xe(bb+64.5))^2);
+        time_diag(bb+64.5)=diag_dist(bb+64.5)/1540; 
+        time_delay(bb+64.5)=time_diag(bb+64.5)-time_diag(65)
+    end
 end 
 
-for dd=1:64
-    xe_2(dd)=((pointTarget.elementSpacingMM)/1000)*dd;
-    diag_dist_2(dd)=sqrt(zf^2 + (xe_2(dd))^2);
-    time_diag_2(dd)=diag_dist_2(dd)/1540;
-    time_delay_2(dd)=time_diag_2(dd)-time_diag_2(1);
-end
-
-xe=[xe_1 xe_2];
-diag_dist=[diag_dist_1 diag_dist_2];
-time_diag=[time_diag_1 time_diag_2];
-time_delay=[time_delay_1 time_delay_2]; 
-
-depth_range_1=[0:zf/63:zf]
-depth_range_2=[zf:-zf/63:0]
-depth_range=[depth_range_1 depth_range_2]
-
-for ee=1:length(depth_range)
-   timeArray(ee)=depth_range(ee)/rate_upsample
-end
-
-for ff=1:length(depth_range)
-timeArray_withDelay(ff)=timeArray(ff)+time_delay(ff)
-end 
-
-for gg=1:128
-    delayedChannel=interp1(timeArray_withDelay,pointTarget.data(:,:,gg),timeArray,'linear')
-    imagesc(interpolation(:,:,gg))
-    colormap('gray')
-end
-
-for 1:128
-    time=time
-
-
-
-
-end
