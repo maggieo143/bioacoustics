c0cb057b1e955d59dbafd9d489af01f4eef1c982
maggieo143 <maggieo143@yahoo.com>
2015-03-09 15:21:08 -0500

    final commit

--- HW2_Q3.m
+++ HW2_Q3.m
@@ -109,5 +109,5 @@ end
 
 figure;
 imagesc(20*log10(abs(hilbert(zone_interp))),[20,80])
-title('Compressed B-Mode Image of Delayed Zone Data [cyst]')
+title('Compressed B-Mode Image of Delayed Zone Data')
 colormap('gray')
